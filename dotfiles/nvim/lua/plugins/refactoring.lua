a5975198356cf8be527f57178e9bafdfbcbc428e
Ville Vartiainen <12277119+vvartiainen@users.noreply.github.com>
2026-04-21 09:33:04 +0300

    fix: refactoring extra to own config for now

--- dotfiles/nvim/lua/plugins/refactoring.lua
+++ dotfiles/nvim/lua/plugins/refactoring.lua
@@ -4,4 +4,23 @@ return {
     "lewis6991/async.nvim",
   },
   lazy = false,
+  keys = {
+    {
+      "<leader>rp",
+      function()
+        return require("refactoring.debug").print_var({ output_location = "below" }) .. "iw"
+      end,
+      mode = { "n", "x" },
+      desc = "Debug Print Variable",
+      expr = true,
+    },
+    {
+      "<leader>rs",
+      function()
+        return require("refactoring").select_refactor()
+      end,
+      mode = { "n", "x" },
+      desc = "Select refactor",
+    },
+  },
 }
