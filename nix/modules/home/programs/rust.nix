923b430fadf9232982d90a789b7785dbb923cf13
Ville Vartiainen <12277119+vvartiainen@users.noreply.github.com>
2026-09-16 12:03:40 +0300

    chore: remove temp

--- nix/modules/home/programs/rust.nix
+++ nix/modules/home/programs/rust.nix
@@ -1,5 +1,4 @@
 {
-  lib,
   pkgs,
   config,
   ...
@@ -18,26 +17,4 @@ in
 
   # cargo install still drops binaries here.
   home.sessionPath = [ cargoBin ];
-
-  # rustup-init left proxy shims in ~/.cargo/bin that would shadow the Nix
-  # toolchain once that directory is on PATH.
-  home.activation.cleanupRustupProxies = lib.hm.dag.entryAfter [ "writeBoundary" ] ''
-    if [[ -e "${cargoBin}/rustup" ]]; then
-      run rm -f \
-        "${cargoBin}/cargo" \
-        "${cargoBin}/cargo-clippy" \
-        "${cargoBin}/cargo-fmt" \
-        "${cargoBin}/cargo-miri" \
-        "${cargoBin}/clippy-driver" \
-        "${cargoBin}/rls" \
-        "${cargoBin}/rust-analyzer" \
-        "${cargoBin}/rust-gdb" \
-        "${cargoBin}/rust-gdbgui" \
-        "${cargoBin}/rust-lldb" \
-        "${cargoBin}/rustc" \
-        "${cargoBin}/rustdoc" \
-        "${cargoBin}/rustfmt" \
-        "${cargoBin}/rustup"
-    fi
-  '';
 }
