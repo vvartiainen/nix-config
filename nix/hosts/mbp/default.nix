80b84738cf834b64f2f30fafa38a0f87156c3738
Ville Vartiainen <12277119+vvartiainen@users.noreply.github.com>
2026-09-18 14:59:01 +0300

    feat: adjust linux builder

--- nix/hosts/mbp/default.nix
+++ nix/hosts/mbp/default.nix
@@ -42,5 +42,23 @@
   system.stateVersion = 6; # This should not be changed even when flakes are updated!
   nixpkgs.hostPlatform = "aarch64-darwin";
 
-  nix.linux-builder.enable = true;
+  nix.linux-builder = {
+    enable = true;
+    package = pkgs.darwin.linux-builder-vz;
+    systems = [
+      "aarch64-linux"
+      "x86_64-linux"
+    ];
+    ephemeral = true;
+    maxJobs = 4;
+    config = {
+      virtualisation = {
+        darwin-builder = {
+          diskSize = 40 * 1024;
+          memorySize = 8 * 1024;
+        };
+        cores = 6;
+      };
+    };
+  };
 }
